a6bdd57d50efb6362c46e8bbedac11e55168aed5
eduardo Henrique <eduardo1214henrique@gmail.com>
2026-05-18 11:53:11 -0300

    Add files via upload

---
+++ dados.php
@@ -0,0 +1,18 @@
+<?php
+
+$avaliacoes = [
+
+    "Bioshock Infinite" => 9.5,
+    "Hylics" => 8.7,
+    "Team Fortress 2" => 9.4,
+    "Elden Ring" => 10,
+    "Fear and Hunger" => 9.0,
+    "Hotline Miami" => 9.3,
+    "Red Dead Redemption 2" => 9.9,
+    "Stardew Valley" => 9.6,
+    "Street Fighter 6" => 9.4,
+    "The King of Fighters 2002" => 9.2
+
+];
+
+?>
